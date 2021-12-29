5ac3120e58f36cb66c7e5f25adbb0c15fb4a1c17
Shiggy <ilarumbgim1@educacion.navarra.es>
2021-12-29 12:50:44 +0100

    base commit

---
+++ anomia/src/components/Header.svelte
@@ -0,0 +1,45 @@
+<script>
+    
+</script>
+
+<div class="text-6xl border m-2 w-max mx-auto px-6">Anomia</div>
+<div class="p-2 w-max mx-auto -my-2">
+  <a
+    class="hover:underline transition-all duration-300 ease-in-out hover:bg-white hover:text-black"
+    href="">Sign In</a
+  >
+  <div class="inline text-lg font-bold">|</div>
+  <a
+    class="hover:underline transition-all duration-300 ease-in-out hover:bg-white hover:text-black"
+    href="">Sign Up</a
+  >
+</div>
+<div class="p-2 space-x-3 text-center mx-1 md:w-max md:mx-auto">
+  <a
+    class="hover:underline transition-all duration-300 ease-in-out hover:bg-white hover:text-black p-1"
+    href="">Home</a
+  >
+  <a
+    class="hover:underline transition-all duration-300 ease-in-out hover:bg-white hover:text-black p-1"
+    href="">SBC</a
+  >
+  <a
+    class="hover:underline transition-all duration-300 ease-in-out hover:bg-white hover:text-black p-1"
+    href="">ETH</a
+  >
+  <a
+    class="hover:underline transition-all duration-300 ease-in-out hover:bg-white hover:text-black p-1"
+    href="">Files</a
+  >
+  <a
+    class="hover:underline transition-all duration-300 ease-in-out hover:bg-white hover:text-black p-1"
+    href="">Privacy</a
+  >
+  <a
+    class="hover:underline transition-all duration-300 ease-in-out hover:bg-white hover:text-black p-1"
+    href="">Moe</a
+  >
+</div>
+
+<style>
+</style>

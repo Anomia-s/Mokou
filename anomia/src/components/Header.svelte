93988ebc3b20cd431f35becd113f889bfaa65c49
Shiggy <ilarumbgim1@educacion.navarra.es>
2021-12-29 18:50:39 +0100

    implement auth :(

--- anomia/src/components/Header.svelte
+++ anomia/src/components/Header.svelte
@@ -1,23 +1,26 @@
 <script>
-    
+import { authenticated, user } from "./auth";
 </script>
 
 <div class="text-6xl border m-2 w-max mx-auto px-6">Anomia</div>
 <div class="p-2 w-max mx-auto -my-2">
+  {#if !$authenticated}
   <a
     class="hover:underline transition-all duration-300 ease-in-out hover:bg-white hover:text-black"
-    href="">Sign In</a
+    href="/auth">Sign In</a
   >
   <div class="inline text-lg font-bold">|</div>
   <a
     class="hover:underline transition-all duration-300 ease-in-out hover:bg-white hover:text-black"
-    href="">Sign Up</a
-  >
+    href="/auth">Sign Up</a
+  >{:else}
+  <div>Hi {$user.username}</div>
+{/if}
 </div>
 <div class="p-2 space-x-3 text-center mx-1 md:w-max md:mx-auto">
   <a
     class="hover:underline transition-all duration-300 ease-in-out hover:bg-white hover:text-black p-1"
-    href="">Home</a
+    href="/">Home</a
   >
   <a
     class="hover:underline transition-all duration-300 ease-in-out hover:bg-white hover:text-black p-1"
--- anomia/src/components/auth.js
+++ anomia/src/components/auth.js
@@ -1,7 +1,90 @@
-import { writable } from "svelte/store";
+import {
+    writable
+} from "svelte/store";
+import { goto } from "@roxi/routify";
 
+export let user = writable({})
+export let authenticated = writable(false)
+const APIEndpoint = "http://127.0.0.1:3333"
 
-export function auth() {
-    
+const routes = {
+    auth: {
+
+        login: "auth/login",
+        register: "auth/register"
+
+    }
 }
 
+let isAuth;
+
+user.subscribe((value) => {
+    sessionStorage.setItem('token', value.token)
+    console.log("💻 user state changed.")
+})
+authenticated.subscribe((variable) => {
+    console.log(variable)
+    isAuth = variable
+    console.log("💻 Authenticated state changed.")
+})
+export function auth() {
+
+}
+
+export function register(username, password) {
+    if (isAuth) {
+        return false;
+    }
+    fetch(`${APIEndpoint}/${routes.auth.register}`, {
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify({
+            username,
+            password
+        })
+    }).then(response => {
+        return response.json()
+    }).then((data) => {
+        if (data.error === true) {
+            console.log('💻 Error!')
+            console.log(data)
+        }
+
+
+    });
+
+    console.log('💻 Request done.')
+
+}
+
+export function logIn(username, password) {
+    if (isAuth) {
+        return false;
+    }
+    fetch(`${APIEndpoint}/${routes.auth.login}`, {
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify({
+            username,
+            password
+        })
+    }).then(response => {
+        return response.json()
+    }).then((data) => {
+        console.log('💻 Authenticated, setting token...')
+        authenticated.set(true)
+        user.set({
+            username: username,
+            token: data.token.token
+        })
+
+        sessionStorage.setItem('token', data.token.token)
+        goto('./index')
+    })
+}
